b9fac37feefcbf2b7cd8337628aecd473e4c00bb
tachimo <shihon17@gmail.com>
2023-01-24 14:43:05 +0000

    Update App.jsx(テキスト反映)

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import React, { useState } from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([
     "ああああ",
     "いいいいい",
   ]);
   const [completeTodos, setCompleteTodos] = useState(["うううう"]);
+
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onChangeTodoText}
+        />
         <button>追加</button>
       </div>
       <div className="incomplete-area">
